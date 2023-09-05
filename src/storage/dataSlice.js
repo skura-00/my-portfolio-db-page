1b00f3ef93253de2b0f060fb6d5cb628478f0d4e
skura-00 <sakura39.081201@gmail.com>
2023-09-05 01:34:06 -0700

    update

--- src/storage/dataSlice.js
+++ src/storage/dataSlice.js
@@ -6,7 +6,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 // insert action
 export const insert = createAsyncThunk('data/insert/', async({address, firstName, lastName, dob, email}) => {
   try {
-    const res = await axios.post('https://main--my-portfolio-db-page.netlify.app/insert', 
+    const res = await axios.post('https://db-page-practice-0f7c8e4a291d.herokuapp.com/insert/', 
     {
       address: address, 
       firstName: firstName, 
@@ -23,7 +23,7 @@ export const insert = createAsyncThunk('data/insert/', async({address, firstName
 
 export const select = createAsyncThunk('data/select/', async() => {
   try {
-    const res = await axios.get('https://main--my-portfolio-db-page.netlify.app/select')
+    const res = await axios.get('https://db-page-practice-0f7c8e4a291d.herokuapp.com/select/')
     return res.data
   } catch (err) {
     console.log(err)
@@ -33,7 +33,7 @@ export const select = createAsyncThunk('data/select/', async() => {
 
 export const selectClub = createAsyncThunk('data/selectClub/', async() => {
   try {
-    const res = await axios.get('https://main--my-portfolio-db-page.netlify.app/select2')
+    const res = await axios.get('https://db-page-practice-0f7c8e4a291d.herokuapp.com/selectClub/')
     return res.data
   } catch (err) {
     console.log(err)
